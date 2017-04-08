70713581daa5564c121c71dc47950a2c1865e0d1
Krzysztof Burkot <buryPL@gmail.com>
2017-04-08 17:01:44 +0200

    Prawie done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict';
+$(function () {
+    var slider = $('#slider');
+    var slideShow = $('.slide_show');
+    var slideCount = $('.slide_show').length;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
+
+    slideShow.css({'width': slideCount*100 + '%'});
+
+    slideShow.find('.single_slide').each(function (index) {
+        $(this).css({
+            'margin-left': index*slideWidth + '%',
+            'width': slideWidth + '%'
+        });
+    });
+    $('.prev_slide').click(function(){slide(slideIndex - 1);});
+    $('.next_slide').click(function(){slide(slideIndex + 1);});
+    
+    function slide (newSlideIndex) {
+        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
+            return;
+        };
+        
+        var slideCaption = slider.find('.slider_caption').eq(newSlideIndex);
+        
+        slideCaption.hide();
+        
+        var marginLeft = (newSlideIndex*(-100)) + '%';
+        
+        slideShow.animate({"margin-left": marginLeft}, 800, function() {
+           slideIndex = newSlideIndex;
+            slideCaption.fadeIn("slow");
+        });
+        
+    };
+})
